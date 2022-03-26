2895c03996ebbf2877f060369f7a689126206908
Fares Hesham <66084853+fares-h-moneim@users.noreply.github.com>
2022-03-26 19:15:38 +0200

    Updated Truck Header

--- Data Structures and Algorithms Project/Truck.h
+++ Data Structures and Algorithms Project/Truck.h
@@ -1,7 +1,28 @@
 #pragma once
 
 class Truck {
-	int MoveTime; //
-	//Fares test
+	int ID;
+	bool Available;
+	static int TruckCount;
+	int Type; //(1) VIP (2) Normal (3) Special
+	int speed;
+	int J; //Number of Journies Before Checkup
+	int CheckUpDuration;
+	
+public:
+
+	Truck(int T, int s, int J, int Duration);
+
+	void SetType(int T);
+	int GetType() const;
+	
+	void SetSpeed(int S);
+	int GetSpeed() const;
+
+	void SetJournies(int J);
+	int GetJournies() const;
+
+	void SetDuration(int Dur);
+	int GetDuration() const;
 
 };
